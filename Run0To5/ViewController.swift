409fb778649d7a8f72887e09c47a128b903dad31
Valeria Duran Ruiz <valeria.do@gmail.com>
2019-05-17 12:17:01 -0500

    Add link for background view to view controller code

---
+++ Run0To5/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Run0To5
+//
+//  Created by Valeria Duran on 5/17/19.
+//  Copyright © 2019 Valeria Duran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //MARK: - Properties
+    @IBOutlet weak var backgroundView: UIView!
+    
+    @IBOutlet weak var playView: UIView!
+    @IBOutlet weak var pauseView: UIView!
+    @IBOutlet weak var mainControlView: UIView!
+    @IBOutlet weak var runTextField: UITextField!
+    @IBOutlet weak var walkTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setupViews()
+    }
+    
+   func setupViews() {
+    //Give backgroundView an image
+    
+    //Build Circles out of square views
+    
+    
+    }
+
+
+}
+
---
+++ Run0To5Tests/Run0To5Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Run0To5Tests.swift
+//  Run0To5Tests
+//
+//  Created by Valeria Duran on 5/17/19.
+//  Copyright © 2019 Valeria Duran. All rights reserved.
+//
+
+import XCTest
+@testable import Run0To5
+
+class Run0To5Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
