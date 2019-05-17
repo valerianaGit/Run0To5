da3db4435b8e93104acbdcb81f0f5fe043b573a7
Valeria Duran Ruiz <valeria.do@gmail.com>
2019-05-17 15:26:07 -0500

    UI- Add background image and round corners in views

--- Run0To5/ViewController.swift
+++ Run0To5/ViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     //MARK: - Properties
-    @IBOutlet weak var backgroundView: UIView!
+    
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var bottomView: UIView!
     
     @IBOutlet weak var playView: UIView!
     @IBOutlet weak var pauseView: UIView!
@@ -27,9 +29,15 @@ class ViewController: UIViewController {
     }
     
    func setupViews() {
-    //Give backgroundView an image
+    //round corners
+    topView.layer.cornerRadius = 10
+    bottomView.layer.cornerRadius = 10
     
     //Build Circles out of square views
+    mainControlView.layer.cornerRadius = 130
+    playView.layer.cornerRadius = 50
+    pauseView.layer.cornerRadius = 50
+    
     
     
     }
