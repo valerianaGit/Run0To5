2f4886a0f20b33fb86fd94c233d14ad312b35024
Valeria Duran Ruiz <valeria.do@gmail.com>
2019-06-06 00:03:33 -0500

    Update timer properties and build base methods to handle timers

--- Run0To5/ViewController.swift
+++ Run0To5/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
     //MARK: - Properties
@@ -18,17 +19,29 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     @IBOutlet weak var timerLabel: UILabel! //update this with the timer,  -  create timer , update it with values chosen from intervals
     @IBOutlet weak var runPauseLabel: UILabel! //just update whether run/walk
     @IBOutlet weak var totalIntervalsToRun: UILabel! //follow which interval we are on
-    
     let runPicker = UIPickerView()
     let walkPicker = UIPickerView()
     var selectedRunIntervals = NSInteger()
     var selectedWalkIntervals = NSInteger()
     var selectedworkoutLength = NSInteger()
-    let timer = Timer()
-    //MARK: - Data Sources - picker views
+   
+    //MARK:  Data Sources - picker views
     let runPickerIntervals = [[1, 1.5, 2, 3, 5, 6, 8, 10, 12, 15, 20, 25, 30, 40, 50, 60], [1, 1.5, 2]]
     let trekRowTitles = ["RUN minutes", "WALK minutes"]
     let totalWorkoutLength = [30, 45, 60, 90, 120]
+    
+    //MARK: Timer
+    var timer = Timer()
+    var alarm = AVAudioPlayer()
+    var walkInterval = 0 //walkPicker[0].chosenitem .ceiling
+    var runInterval = 2 //walkPicker[1].chosenitem .ceiling
+    var totalNumberOfIntervals = 8 //  totalWorkoutPicker / (walk + run ) .ceiling
+    enum IntervalType {
+        case Run
+        case Walk
+    }
+    let intervals: [IntervalType] = [.Run, .Walk]
+    
     //MARK: - Methods
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,6 +49,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         setupPlayAndPauseButtons()
         runTextField.delegate = self
         walkTextField.delegate = self
+        soundAlarm()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -57,6 +71,22 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     buildPickerViews()
     addToolBarToPickerViews()
  }
+    
+    //MARK: - TIMER Methods
+    func soundAlarm() {
+        do {
+            alarm = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "alarm", ofType: "mp3")!))
+            alarm.prepareToPlay()
+            let audioSession = AVAudioSession.sharedInstance()
+            do {
+                try audioSession.setCategory(AVAudioSession.Category.playback)
+            } catch {
+                print(error)
+            }
+        } catch {
+            print("Couldn't play alarm\(error)")
+        }
+    }
     //MARK: - Play and Pause buttons methods
     func setupPlayAndPauseButtons() {
         let playTap = UITapGestureRecognizer(target: self, action: #selector(handlePlayTap))
@@ -74,12 +104,67 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         //new timer for intervals, each interval is a new instance fo the timer, because it has to represent the time needed,
         //everytime a walk interval happens, the label on bottom of circle updates to reflect how many we have done/ how many left
         //build the timer to fire at the required intervals
-        
-        
+        playPauseBehavior()
         
     }
     @objc func handlePauseTap() {
         print("pause was tapped")
+        if timer.isValid {
+            timer.invalidate()
+        }
+           }
+    
+ 
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(runningTimer),userInfo: nil, repeats: true)
+    }
+    
+    @objc func runningTimer() {
+//        if timeRemaining > 0 && currentInterval == 0 /*&& currentTomato == 0 */{
+//            print("running rogue")
+//            startNextInterval()
+//        } else if timeRemaining > 0 {
+//            print("running timer phase 1 got called")
+//            timeRemaining -= 1
+//            print("time: \(timeRemaining)")
+//        } else if timeRemaining == 0 {
+//            alarm.play()
+//            timer.invalidate()
+//            //if playButton is pressed, start next interval
+//            startNextInterval()
+//            print("running timer phase 2 got called")
+//        }
+//        updateUI()
+       // perform(#selector(ViewController.updateUI), with: nil, afterDelay: 0.0)
+    }
+    
+    @objc func startNextInterval() {
+//        print("current interval : \(currentInterval)")
+//        if currentInterval < intervals.count {
+//            if intervals[currentInterval] == .Pomodoro {
+//                timeRemaining = tomatoTimer
+//                let tomatoes = (currentInterval + 2) / 2
+//                print("\(tomatoes) tomatoes")
+//                updateTomatoes()
+//            } else if intervals[currentInterval] == .RestBreak {
+//                timeRemaining = breakTimer
+//            } else if intervals[currentInterval] == .LongBreak {
+//                timeRemaining = longBreakTimer
+//            }
+//            currentInterval += 1
+//        } else {
+//            currentInterval = 0
+//        }
+    }
+    func playPauseBehavior() {
+        if timer.isValid {
+            // playButton.setTitle("Resume", for: .normal)
+            //resetBarButton.isEnabled = true
+            // handleCircleCompletionTap()
+            handlePauseTap()
+        } else {
+            startTimer()
+        }
     }
     
     //MARK: - TextField Delegate
